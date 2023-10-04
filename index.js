975e1f7cafd80820318f7c7be5ac5d4b0eb16d6f
Youngtechie <olaegbeolamilekan1@gmail.com>
2023-10-04 13:00:52 +0100

    changes::

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const url = "http://127.0.0.1:5500/results-summary-component-main/data.json";
-let data = [];
-async function getResultsSummary() {
-  await fetch(url).then((res) => {
-    res.json().then((result) => {
-      data = result;
-      let ul = document.body.querySelector("ul");
-      for (var i = 0; i < data.length; i++) {
-        let text = `
+import summary from "./data.js";
+
+function getResultsSummary() {
+  let ul = document.body.querySelector("ul");
+  for (var i = 0; i < summary.length; i++) {
+    let text = `
                 <div class="iconNText">
-                <img class="icon" src='${data[i].icon}'/>
-                <span> ${data[i].category} </span>
+                <img class="icon" src='${summary[i].icon}'/>
+                <span> ${summary[i].category} </span>
               </div>
               <span>
-                <span class="bold">${data[i].score}</span> <span class="dull">/ 100</span>
+                <span class="bold">${summary[i].score}</span> <span class="dull">/ 100</span>
               </span>`;
 
-        const li = document.createElement("li");
-        
-        li.innerHTML = text;
+    const li = document.createElement("li");
 
-        ul.append(li);
-      }
-    });
-  });
+    li.innerHTML = text;
+
+    ul.append(li);
+  }
 }
 
 getResultsSummary();
